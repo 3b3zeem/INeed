adb47fb0bf92dd95e2d3286c81e92ad3549e52eb
3b3zeem <ahmedzezo2247@gmail.com>
2025-05-29 15:48:43 +0300

    INeed desktop

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import INeed from './INeed/INeed'
+import PreventImageDragAndRightClick from './PreventImageDragAndRightClick';
+
+function App() {
+
+  return (
+    <>
+      <INeed />
+      <PreventImageDragAndRightClick />
+    </>
+  )
+}
+
+export default App
